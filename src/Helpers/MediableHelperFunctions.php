7dacea4e5a6d01d78ea8aba66ba5244fff6c57b7
ah_mabrouk <ahmed.mabrouk@fndevcs.com>
2020-09-24 14:13:49 +0200

    check helper functions if exists to avoid conflict

--- src/Helpers/MediableHelperFunctions.php
+++ src/Helpers/MediableHelperFunctions.php
@@ -2,30 +2,32 @@
 
 use Illuminate\Support\Str;
 
-function getYoutubeVideoId($path)
-{
-    $possibleVideoIdPrefix = [
-        'youtu.be/',
-        'embed/',
-        '?vi=',
-        '&v=',
-        '?v=',
-        '/vi/',
-        '/v/'
-    ];
+if (! function_exists('getYoutubeVideoId')) {
+    function getYoutubeVideoId($path) {
+        $possibleVideoIdPrefix = [
+            'youtu.be/',
+            'embed/',
+            '?vi=',
+            '&v=',
+            '?v=',
+            '/vi/',
+            '/v/'
+        ];
 
-    $videoId = $path;
-    foreach ($possibleVideoIdPrefix as $case) {
-        if (Str::contains($videoId, $case)) {
-            $videoId = explode($case, $videoId)[1];
+        $videoId = $path;
+        foreach ($possibleVideoIdPrefix as $case) {
+            if (Str::contains($videoId, $case)) {
+                $videoId = explode($case, $videoId)[1];
+            }
         }
+        return explode('?', explode('&', $videoId)[0])[0];
     }
-    return explode('?', explode('&', $videoId)[0])[0];
 }
 
-function randomBy($num = 6)
-{
-    $start = (int) str_repeat(1, $num);
-    $end = (int) str_repeat(9, $num);
-    return rand($start, $end);
+if (! function_exists('randomBy')) {
+    function randomBy($num = 6) {
+        $start = (int) str_repeat(1, $num);
+        $end = (int) str_repeat(9, $num);
+        return rand($start, $end);
+    }
 }
