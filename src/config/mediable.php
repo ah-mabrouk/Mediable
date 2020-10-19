65a47eace0000525bc1b4f19e28369c4c7f55b32
ah_mabrouk <ahmed.mabrouk@fndevcs.com>
2020-10-19 11:46:41 +0200

    fix config values path

--- src/config/mediable.php
+++ src/config/mediable.php
@@ -2,34 +2,14 @@
 
 return [
     'photos' => [
-        'path' => env('APP_PHOTOS_PATH'), 'public/photos/',
+        'path' => env('APP_PHOTOS_PATH', 'public/photos/'),
     ],
 
     'files' => [
-        'path' => env('APP_FILES_PATH'), 'public/files/',
+        'path' => env('APP_FILES_PATH', 'public/files/'),
     ],
 
     'videos' => [
-        'path' => env('APP_VIDEOS_PATH'), 'public/videos/',
-    ],
-
-    'prefix' => '',
-
-    'middleware' => [
-        //
-    ],
-
-    /**
-     * syntax [
-     *      'specific_directory_path_included_in_media_file_path' => [
-     *          'middleware_1',
-     *          'middleware_2',
-     *      ],
-     * ]
-     * each path may apply its own middlewares
-     * active only when reading a media file from storage
-     */
-    'protected_internal_media_base_paths' => [
-        // '' => [],
+        'path' => env('APP_VIDEOS_PATH', 'public/videos/'),
     ],
 ];
