372aa65645abf056637594d93c2b114dcffc5b27
Ahmed-Mabrouk <a.mabrouk25@gmail.com>
2023-06-11 20:38:48 +0300

    fix factory

--- src/database/factories/MediaFactory.php
+++ src/database/factories/MediaFactory.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace Database\Factories;
+namespace Mabrouk\Mediable\Database\Factories;
 
+use Mabrouk\Mediable\Models\Media;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +10,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class MediaFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Media::class;
+
     /**
      * Define the model's default state.
      *
